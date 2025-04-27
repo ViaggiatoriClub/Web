4325348f8fd17333a305c0fdc0f2c11687bc7bfc
V.C <109681262+ViaggiatoriClub@users.noreply.github.com>
2025-04-27 18:20:57 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1745790890,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/articulosycomunicados-sheet0.png",
+		"images/reseñasyexperiencias-sheet0.png",
+		"images/background-sheet0.png",
+		"images/margen.png",
+		"images/whatsapp-sheet0.png",
+		"images/instagram-sheet0.png",
+		"images/restooltip.png",
+		"images/res2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
